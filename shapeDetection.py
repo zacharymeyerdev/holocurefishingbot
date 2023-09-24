05753685e1d60a624d547a219b38515e67570092
zacharymeyerdev <zacharymeyer100@gmail.com>
2023-09-24 18:19:50 -0500

    ssft

--- shapeDetection.py
+++ shapeDetection.py
@@ -21,18 +21,14 @@ for shape, url in image_urls.items():
         resp = urllib.request.urlopen(url)
         image = np.asarray(bytearray(resp.read()), dtype="uint8")
         image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-        
-        # Calculate Hu Moments
         moments = cv2.moments(image)
         hu_moments = cv2.HuMoments(moments)
         reference_hu_moments[shape] = hu_moments
-    except urllib.error.HTTPError as e:
-        print(f"HTTPError: {e.code} for url: {url}")
-    except urllib.error.URLError as e:
-        print(f"URLError: {e.reason} for url: {url}")
+    except Exception as e:
+        print(f"Error loading image for {shape} from {url}: {e}")
 
 # Define the region of interest (x, y, width, height)
-roi = (1131, 687, 96, 120)
+roi = (276, 242, 133, 38)
 
 # Define shape descriptors
 shapes = {
@@ -48,38 +44,23 @@ def compare_shapes(hu_moments1, hu_moments2):
 
 while True:
     with mss.mss() as sct:
-        for monitor_number, monitor in enumerate(sct.monitors[1:], start=1):
-            print(f"Monitor {monitor_number}: {monitor}")
-        monitor = sct.monitors[1]  # or 2, 3, etc. based on which monitor you want to capture
-        while True:
-            # Capture the screenshot of the specified monitor
-            screen = np.array(sct.shot(output='sct.png', screen=monitor))
-            # ... rest of your code
+        monitor = sct.monitors[1]
+        screen = np.array(sct.grab(monitor))
 
-        
-    # Extract the region of interest
     roi_image = screen[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
-    
-    # Convert to grayscale and apply thresholding
     gray = cv2.cvtColor(roi_image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    
-    # Find contours
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+
     for contour in contours:
-        # Create a binary mask of the contour
         mask = np.zeros_like(gray)
         cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
-        
-        # Calculate Hu Moments for the detected contour
         moments = cv2.moments(mask)
         detected_hu_moments = cv2.HuMoments(moments)
-        
-        # Compare with reference Hu Moments
+
         for shape, hu_moments in reference_hu_moments.items():
             if compare_shapes(detected_hu_moments, hu_moments):
                 key = shapes.get(shape)
                 if key:
                     pyautogui.press(key)
-                break  # Break if a shape is found to avoid pressing multiple keys
+                break
